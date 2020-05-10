e051a71898a2a93eeba3a491d848c31bcbbf956c
mattya-lab <61013032+mattya-lab@users.noreply.github.com>
2020-05-10 11:24:07 +0900

    Add files via upload

---
+++ 2015Preliminary/P01.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < n; i++)
+
+int main(void){
+  int p, m, c; cin >> p >> m >> c;
+  cout << ( p + m + c ) << endl;
+  return 0;
+}
